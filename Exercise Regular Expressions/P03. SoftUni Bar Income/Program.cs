8f92df61d36aa962fbb0216ebcb6a6fd474281ff
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-03-25 15:08:29 +0200

    Task 3 initialization

---
+++ Exercise Regular Expressions/P03. SoftUni Bar Income/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P03._SoftUni_Bar_Income
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
