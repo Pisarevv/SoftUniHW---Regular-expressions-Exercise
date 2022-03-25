373ad0bbd1de10b898765b7632ec630880f9c0eb
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-03-25 14:17:49 +0200

    Update

--- Exercise Regular Expressions/P02. Race/Program.cs
+++ Exercise Regular Expressions/P02. Race/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace P02._Race
 {
@@ -6,7 +9,30 @@ namespace P02._Race
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string,int> participants = new Dictionary<string,int>();
+
+            string patternName = @"(?<name>[A-Za-z]+)";
+            string patternDistance = @"(?<distance>\d+)";
+
+            string participtians = Console.ReadLine();
+            string input = string.Empty;
+
+            while((input = Console.ReadLine()) != "end of race")
+            {
+                Match participtian = Regex.Match(input, patternName);
+                Match distance = Regex.Match(input, patternDistance);
+                if (participtian.Success && distance.Success)
+                {
+                    string name = participtian.Groups["name"].Value;
+                    string distanceTraveled = distance.Groups["distance"].Value;
+
+                    if (participants.ContainsKey(name))
+                    {
+                        participants[participtian.Value]
+                    }
+                }
+            }
+
         }
     }
 }
