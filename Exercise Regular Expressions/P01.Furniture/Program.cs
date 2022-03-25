344713d4c7788820ad4681aa072ea3e962bc936a
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-03-25 14:02:25 +0200

    Final update task 1

--- Exercise Regular Expressions/P01.Furniture/Program.cs
+++ Exercise Regular Expressions/P01.Furniture/Program.cs
@@ -15,9 +15,23 @@ namespace P01.Furniture
             List<string> boughtFurniture = new List<string>();
             while ((input = Console.ReadLine()) != "Purchase")
             {
-               
+               Match match = Regex.Match(input, pattern);
+                if (match.Success)
+                {
+                    string furnitureType = match.Groups["furnitureType"].Value;
+                    string price = match.Groups["price"].Value;
+                    string quantity = match.Groups["quantity"].Value;
+                    boughtFurniture.Add(furnitureType);
+                    finalPrice += decimal.Parse(price) * int.Parse(quantity);
+                }
             }
-            
+
+            Console.WriteLine("Bought furniture:");
+            foreach (var item in boughtFurniture)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Total money spend: {finalPrice:F2}");
         }
     }
 }
