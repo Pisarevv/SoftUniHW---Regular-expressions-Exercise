333b616ef74a20c0c3534c8269849c4a438aad23
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-03-26 19:55:32 +0200

    Update but with a bug in output print

--- Exercise Regular Expressions/P03. SoftUni Bar Income/Program.cs
+++ Exercise Regular Expressions/P03. SoftUni Bar Income/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace P03._SoftUni_Bar_Income
 {
@@ -6,7 +7,27 @@ namespace P03._SoftUni_Bar_Income
     {
         static void Main(string[] args)
         {
+            
             string pattern = @"(?<name>\%[A-Z]{1}[a-z]+)\%(([^\|\$\%\.]+)*?)<(?<product>[A-Za-z]+)>([^\|\$\%\.]+)*?\|(?<quantity>\d+)\|([^\|\$\%\.]+)*?(?<price>\d+(\.\d)*?)\$";
+            decimal finalPrice = 0;
+
+            string input = string.Empty;
+
+            while((input = Console.ReadLine()) != "end of shift")
+            {
+                Match match = Regex.Match(input, pattern);
+                if (match.Success)
+                {
+                    string customerName = match.Groups["name"].Value;
+                    string product = match.Groups["product"].Value;
+                    int quantity = int.Parse(match.Groups["quantity"].Value);
+                    decimal price = decimal.Parse(match.Groups["price"].Value);
+                    finalPrice += quantity * price;
+                    Console.WriteLine($"{customerName}: {product} - {price*quantity:F2}");
+                }
+            }
+            Console.WriteLine($"Total income: {finalPrice:F2}");
         }
+
     }
 }
