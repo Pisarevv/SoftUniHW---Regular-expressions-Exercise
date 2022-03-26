46e64fa7c2f570c3d1fceb7e1c9a45c364a5557b
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-03-26 20:48:23 +0200

    Decryption

--- Exercise Regular Expressions/P04.Star Enigma/Program.cs
+++ Exercise Regular Expressions/P04.Star Enigma/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace P04.Star_Enigma
 {
@@ -6,7 +7,36 @@ namespace P04.Star_Enigma
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+               string encryptedMassage = Console.ReadLine();
+               string decryptedMassage = DecryptingMassage(encryptedMassage);
+            }
+            
+        }
+
+        public static string DecryptingMassage (string input)
+        {
+            string encryptedMassage = input;
+            string decryptedMassage = string.Empty;
+            string pattern = @"[star]";
+
+            MatchCollection matches = Regex.Matches(encryptedMassage, pattern,RegexOptions.IgnoreCase);
+            int decriptionKey = matches.Count;
+
+            char[] encryptedMassageCr = encryptedMassage.ToCharArray();
+            for (int i = 0; i < encryptedMassageCr.Length; i++)
+            {
+                int currenctChar = encryptedMassageCr[i] - decriptionKey;
+                char currChar = (char)currenctChar;
+                decryptedMassage += currChar;
+            }
+
+
+            return encryptedMassage;
+
         }
     }
+
 }
