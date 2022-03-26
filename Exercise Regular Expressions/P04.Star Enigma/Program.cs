947d092e405bd5faa000935980d1edad7393e12e
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-03-26 21:22:26 +0200

    Task 4 final update

--- Exercise Regular Expressions/P04.Star Enigma/Program.cs
+++ Exercise Regular Expressions/P04.Star Enigma/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace P04.Star_Enigma
@@ -8,22 +9,46 @@ namespace P04.Star_Enigma
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
+            List<string> attackedPlanets = new List<string>();
+            List<string> destroyedPlanets = new List<string>();
             for (int i = 0; i < n; i++)
             {
                 string encryptedMassage = Console.ReadLine();
                 string decryptedMassage = DecryptingMassage(encryptedMassage);
-                string pattern = @"\@(?<planet>[A-Za-z]+).?\:(?<population>\d+).?\!(?<attackType>\w{1}).?\-\>(?<soldierCount>\d+)";
+                string pattern = @"[^\@\-\!\:\>]*?\@(?<planet>[A-Za-z]+)[^\@\-\!\:\>]*?\:(?<population>\d+)[^\@\-\!\:\>]*?\!(?<attackType>\w+)\![^\@\-\!\:\>]*?\-\>(?<soldiersCount>\d+)[^\@\-\!\:\>]*?";
 
                 Match match = Regex.Match(decryptedMassage, pattern);
-               
+                if (match.Success)
+                {
                     string planetName = match.Groups["planet"].Value;
-                    string population = match.Groups["population"].Value;
+                    int population = int.Parse(match.Groups["population"].Value);
                     string attackType = match.Groups["attackType"].Value;
-                    string soldierCount = match.Groups["soldierCount"].Value;
-               
+                    int soldierCount =int.Parse(match.Groups["soldiersCount"].Value);
+
+                    if (attackType == "A")
+                    {
+                        attackedPlanets.Add(planetName);
+                    }
+                    else if (attackType == "D")
+                    {
+                        destroyedPlanets.Add(planetName);
+                    }
+                };
 
             }
-            
+            attackedPlanets.Sort();
+            destroyedPlanets.Sort();
+            Console.WriteLine($"Attacked planets: {attackedPlanets.Count}");
+            foreach (var planet in attackedPlanets)
+            {
+                Console.WriteLine($"-> {planet}");
+            }
+            Console.WriteLine($"Destroyed planets: {destroyedPlanets.Count}");
+            foreach (var planet in destroyedPlanets)
+            {
+                Console.WriteLine($"-> {planet}");
+            }
+
         }
 
         public static string DecryptingMassage (string input)
