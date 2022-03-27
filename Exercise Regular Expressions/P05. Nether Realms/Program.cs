32faa0c72af59f41491deced98309a2df3f46d99
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-03-27 22:59:44 +0300

    Regex update

--- Exercise Regular Expressions/P05. Nether Realms/Program.cs
+++ Exercise Regular Expressions/P05. Nether Realms/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace P05._Nether_Realms
 {
@@ -6,7 +8,32 @@ namespace P05._Nether_Realms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Demon> demons = new List<Demon>();
+            string namePattern = @"(?<name>[A-Za-z]+)";
+            string damagePattern = @"(?<digits>(\-|\+)*?\d+(\.\d+)*)|(?<operators>[\*\\]+)";
+            string demonInput = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(demonInput, namePattern);
+
+            foreach (Match match in matches)
+            {
+                string demonName = match.Groups["name"].Value;
+            }
+
         }
+    }
+
+    public class Demon
+    {
+        public Demon(string name, double health, double damage)
+        {
+            this.Name = name;
+            this.Health = health;
+            this.DamagePoints = damage;
+        }
+        public string Name { get; set; }
+        public double Health { get; set; }
+        public double DamagePoints { get; set; }
+
     }
 }
