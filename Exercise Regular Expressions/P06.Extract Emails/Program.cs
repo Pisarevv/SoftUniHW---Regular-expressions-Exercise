25301c8bf7c602ef7d1d664baf1eacbf8648fa69
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-03-28 20:43:02 +0300

    Task 6 initialization

---
+++ Exercise Regular Expressions/P06.Extract Emails/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P06.Extract_Emails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
